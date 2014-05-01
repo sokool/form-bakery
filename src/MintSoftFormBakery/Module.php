8d5d294f5920f3071d0e913ed40a18a0a41bba0d
Michał Sokołowski <michal.sokolowski@smtsoftware.com>
2014-05-01 11:06:23 +0200

    base settings

---
+++ src/MintSoftFormBakery/Module.php
@@ -0,0 +1,41 @@
+<?php
+namespace MintSoftFormBakery;
+
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    ConfigProviderInterface,
+    AutoloaderProviderInterface,
+    BootstrapListenerInterface,
+    ServiceProviderInterface
+{
+
+    public function onBootstrap(EventInterface $e)
+    {
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/config/service.config.php';
+    }
+}
